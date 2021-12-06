509ef343e673294d3407043fda8754c797f7b181
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2021-12-06 14:54:10 +0100

    Updated PhotoCarousel.Display POC

--- PhotoCarousel/PhotoCarousel.Display/ViewModels/MainWindowViewModel.cs
+++ PhotoCarousel/PhotoCarousel.Display/ViewModels/MainWindowViewModel.cs
@@ -1,33 +1,74 @@
 using Avalonia.Media.Imaging;
 using PhotoCarousel.Display.Models;
+using ReactiveUI;
 using System;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace PhotoCarousel.Display.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private readonly SynchronizationContext _synchronizationContext = SynchronizationContext.Current;
+        private readonly PeriodicTimer _timer = new PeriodicTimer(TimeSpan.FromMinutes(1));
+
         private HttpClient _httpClient;
 
         public string Greeting => "Welcome to Avalonia!";
 
-        public IBitmap TestImage { get; set; }
+        private IBitmap _testImage;
+        public IBitmap TestImage
+        {
+            get
+            {
+                return _testImage;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _testImage, value);
+            }
+        }
 
-        public string TestDescription { get; set; }
+        private string _testDescription;
+        public string TestDescription
+        {
+            get
+            {
+                return _testDescription;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _testDescription, value);
+            }
+        }
 
         public MainWindowViewModel()
         {
-            _httpClient = new HttpClient();
-            _httpClient.BaseAddress = new Uri("http://192.168.10.2:8077");
+            _ = Task.Run(async () =>
+            {
+                do
+                {
+                    try
+                    {
+                        _httpClient = new HttpClient();
+                        _httpClient.BaseAddress = new Uri("http://192.168.10.2:8077");
 
-            var response = _httpClient.GetFromJsonAsync<Photo>("photos/random").Result;
-            var photo = _httpClient.GetByteArrayAsync($"downloads/photo/{response.Id}").Result;
+                        var response = _httpClient.GetFromJsonAsync<Photo>("photos/random").Result;
+                        var photo = _httpClient.GetByteArrayAsync($"downloads/photo/{response.Id}").Result;
 
-            using var stream = new MemoryStream(photo);
-            TestImage = new Bitmap(stream);
-            TestDescription = response.Description;
+                        _synchronizationContext.Post((x) =>
+                        {
+                            using var stream = new MemoryStream(photo);
+                            TestImage = new Bitmap(stream);
+                            TestDescription = response.Description;
+                        }, null);
+                    }
+                    catch { }
+                } while (await _timer.WaitForNextTickAsync());
+            });
         }
     }
 }
