e9717ca273074c8780a7ae8987770f688992b0db
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2021-12-23 14:20:54 +0100

    Tries a different passepartout for PhotoCarousel.DIsplay

--- PhotoCarousel/PhotoCarousel.Display/ViewModels/MainWindowViewModel.cs
+++ PhotoCarousel/PhotoCarousel.Display/ViewModels/MainWindowViewModel.cs
@@ -13,51 +13,37 @@ namespace PhotoCarousel.Display.ViewModels
     public class MainWindowViewModel : ViewModelBase
     {
         private readonly SynchronizationContext _synchronizationContext = SynchronizationContext.Current;
-        private readonly PeriodicTimer _timer = new PeriodicTimer(TimeSpan.FromMinutes(1));
-
-        private HttpClient _httpClient;
-
-        public string Greeting => "Welcome to Avalonia!";
+        private readonly PeriodicTimer _timer = new(TimeSpan.FromMinutes(5));
 
         private IBitmap _testImage;
         public IBitmap TestImage
         {
-            get
-            {
-                return _testImage;
-            }
-            set
-            {
-                this.RaiseAndSetIfChanged(ref _testImage, value);
-            }
+            get => _testImage;
+            set => this.RaiseAndSetIfChanged(ref _testImage, value);
         }
 
         private string _testDescription;
         public string TestDescription
         {
-            get
-            {
-                return _testDescription;
-            }
-            set
-            {
-                this.RaiseAndSetIfChanged(ref _testDescription, value);
-            }
+            get => _testDescription;
+            set => this.RaiseAndSetIfChanged(ref _testDescription, value);
         }
 
         public MainWindowViewModel()
         {
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://192.168.10.2:8077");
+
             _ = Task.Run(async () =>
             {
                 do
                 {
                     try
                     {
-                        _httpClient = new HttpClient();
-                        _httpClient.BaseAddress = new Uri("http://192.168.10.2:8077");
 
-                        var response = _httpClient.GetFromJsonAsync<Photo>("photos/random").Result;
-                        var photo = _httpClient.GetByteArrayAsync($"downloads/photo/{response.Id}").Result;
+
+                        var response = httpClient.GetFromJsonAsync<Photo>("photos/random").Result;
+                        var photo = httpClient.GetByteArrayAsync($"downloads/photo/{response.Id}").Result;
 
                         _synchronizationContext.Post((x) =>
                         {
