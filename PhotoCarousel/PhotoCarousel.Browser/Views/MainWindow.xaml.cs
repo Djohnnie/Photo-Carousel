ef7ee219a0970f7e052cf6267a7b8ca95cbdb1ba
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2022-01-21 15:41:22 +0100

    Worked on versioning

--- PhotoCarousel/PhotoCarousel.Browser/Views/MainWindow.xaml.cs
+++ PhotoCarousel/PhotoCarousel.Browser/Views/MainWindow.xaml.cs
@@ -1,16 +1,17 @@
-﻿using System.Windows;
+﻿using System.Reflection;
+using System.Windows;
 using PhotoCarousel.Browser.ViewModels;
 
 namespace PhotoCarousel.Browser.Views;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
         DataContext = new MainViewModel();
+        
+        var version = Assembly.GetExecutingAssembly().GetName().Version;
+        Title = $"{Title} ({version})";
     }
 }
--- PhotoCarousel/PhotoCarousel.Browser/setup.iss
+++ PhotoCarousel/PhotoCarousel.Browser/setup.iss
@@ -23,7 +23,7 @@ DisableProgramGroupPage=yes
 ; Uncomment the following line to run in non administrative install mode (install for current user only.)
 ;PrivilegesRequired=lowest
 PrivilegesRequiredOverridesAllowed=dialog
-OutputBaseFilename=PhotoCarousel.Browser.Setup
+OutputBaseFilename=PhotoCarousel.Browser.{#MyAppVersion}.Setup
 Compression=lzma
 SolidCompression=yes
 WizardStyle=modern
