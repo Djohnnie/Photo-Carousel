953bbdcf2e63c9c3bfa8715281aee480087695b5
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2022-03-27 22:10:55 +0200

    Added missing DateTimeExtensions

---
+++ PhotoCarousel/PhotoCarousel.Common/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PhotoCarousel.Common.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime RoundToMinutes(this DateTime value)
+        {
+            return new DateTime(value.Year, value.Month, value.Day, value.Hour, value.Minute, 0, 0, value.Kind);
+        }
+    }
+}
