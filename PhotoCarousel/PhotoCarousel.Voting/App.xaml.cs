b9162203be69bffcd33adaea5a407e3051e7dff7
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2022-03-28 11:01:01 +0200

    Added MAUI project for voting application

---
+++ PhotoCarousel/PhotoCarousel.Voting/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PhotoCarousel.Voting
+{
+    public partial class App : Application
+    {
+        public App(MainPage mainPage)
+        {
+            InitializeComponent();
+
+            MainPage = mainPage;
+        }
+    }
+}
---
+++ PhotoCarousel/PhotoCarousel.Voting/MauiProgram.cs
@@ -0,0 +1,22 @@
+﻿using PhotoCarousel.Voting.ViewModels;
+
+namespace PhotoCarousel.Voting
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+            builder.Services.AddSingleton<MainPage>();
+            builder.Services.AddSingleton<MainViewModel>();
+
+            return builder.Build();
+        }
+    }
+}
