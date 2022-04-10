a558214b5e411403af1b4c0aa31401d1d9310dd7
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2022-04-10 16:56:05 +0200

    Tried to debug on Android phone

--- PhotoCarousel/PhotoCarousel.Voting/ViewModels/MainViewModel.cs
+++ PhotoCarousel/PhotoCarousel.Voting/ViewModels/MainViewModel.cs
@@ -1,285 +1,282 @@
 ﻿using PhotoCarousel.Enums;
 using PhotoCarousel.Voting.Helpers;
 using PhotoCarousel.Voting.ViewModels.Base;
-using System.Diagnostics;
 using System.Windows.Input;
 
-namespace PhotoCarousel.Voting.ViewModels
+namespace PhotoCarousel.Voting.ViewModels;
+
+public class MainViewModel : ViewModelBase
 {
-    public class MainViewModel : ViewModelBase
+    private readonly ApiClientHelper _apiClientHelper;
+    private readonly PeriodicTimer _timer = new(TimeSpan.FromMinutes(1));
+
+    private string _errorDescription;
+    public string ErrorDescription
     {
-        private readonly ApiClientHelper _apiClientHelper;
-        private readonly PeriodicTimer _timer = new(TimeSpan.FromMinutes(1));
-
-        private string _errorDescription;
-        public string ErrorDescription
+        get => _errorDescription;
+        set
         {
-            get => _errorDescription;
-            set
-            {
-                _errorDescription = value;
-                OnPropertyChanged(nameof(ErrorDescription));
-            }
+            _errorDescription = value;
+            OnPropertyChanged(nameof(ErrorDescription));
         }
+    }
 
-        private Guid _previousPhotoId;
-        private byte[] _previousPhoto;
-        public byte[] PreviousPhoto
+    private Guid _previousPhotoId;
+    private byte[] _previousPhoto;
+    public byte[] PreviousPhoto
+    {
+        get => _previousPhoto;
+        set
         {
-            get { return _previousPhoto; }
-            set
-            {
-                _previousPhoto = value;
-                OnPropertyChanged(nameof(PreviousPhoto));
-            }
+            _previousPhoto = value;
+            OnPropertyChanged(nameof(PreviousPhoto));
         }
+    }
 
-        private Rating _previousPhotoRating;
-        public Rating PreviousPhotoRating
+    private Rating _previousPhotoRating;
+    public Rating PreviousPhotoRating
+    {
+        get => _previousPhotoRating;
+        set
         {
-            get { return _previousPhotoRating; }
-            set
-            {
-                _previousPhotoRating = value;
-                OnPropertyChanged(nameof(PreviousPhotoRating));
-            }
+            _previousPhotoRating = value;
+            OnPropertyChanged(nameof(PreviousPhotoRating));
         }
+    }
 
-        private string _previousPhotoDescription;
-        public string PreviousPhotoDescription
+    private string _previousPhotoDescription;
+    public string PreviousPhotoDescription
+    {
+        get => _previousPhotoDescription;
+        set
         {
-            get => _previousPhotoDescription;
-            set
-            {
-                _previousPhotoDescription = value;
-                OnPropertyChanged(nameof(PreviousPhotoDescription));
-            }
+            _previousPhotoDescription = value;
+            OnPropertyChanged(nameof(PreviousPhotoDescription));
         }
+    }
 
-        private Guid _currentPhotoId;
-        private byte[] _currentPhoto;
-        public byte[] CurrentPhoto
+    private Guid _currentPhotoId;
+    private byte[] _currentPhoto;
+    public byte[] CurrentPhoto
+    {
+        get => _currentPhoto;
+        set
         {
-            get { return _currentPhoto; }
-            set
-            {
-                _currentPhoto = value;
-                OnPropertyChanged(nameof(CurrentPhoto));
-            }
+            _currentPhoto = value;
+            OnPropertyChanged(nameof(CurrentPhoto));
         }
+    }
 
-        private Rating _currentPhotoRating;
-        public Rating CurrentPhotoRating
+    private Rating _currentPhotoRating;
+    public Rating CurrentPhotoRating
+    {
+        get => _currentPhotoRating;
+        set
         {
-            get { return _currentPhotoRating; }
-            set
-            {
-                _currentPhotoRating = value;
-                OnPropertyChanged(nameof(CurrentPhotoRating));
-            }
+            _currentPhotoRating = value;
+            OnPropertyChanged(nameof(CurrentPhotoRating));
         }
+    }
 
-        private string _currentPhotoDescription;
-        public string CurrentPhotoDescription
+    private string _currentPhotoDescription;
+    public string CurrentPhotoDescription
+    {
+        get => _currentPhotoDescription;
+        set
         {
-            get => _currentPhotoDescription;
-            set
-            {
-                _currentPhotoDescription = value;
-                OnPropertyChanged(nameof(CurrentPhotoDescription));
-            }
+            _currentPhotoDescription = value;
+            OnPropertyChanged(nameof(CurrentPhotoDescription));
         }
+    }
 
-        private Guid _nextPhotoId;
-        private byte[] _nextPhoto;
-        public byte[] NextPhoto
+    private Guid _nextPhotoId;
+    private byte[] _nextPhoto;
+    public byte[] NextPhoto
+    {
+        get => _nextPhoto;
+        set
         {
-            get { return _nextPhoto; }
-            set
-            {
-                _nextPhoto = value;
-                OnPropertyChanged(nameof(NextPhoto));
-            }
+            _nextPhoto = value;
+            OnPropertyChanged(nameof(NextPhoto));
         }
+    }
 
-        private Rating _nextPhotoRating;
-        public Rating NextPhotoRating
+    private Rating _nextPhotoRating;
+    public Rating NextPhotoRating
+    {
+        get => _nextPhotoRating;
+        set
         {
-            get { return _nextPhotoRating; }
-            set
-            {
-                _nextPhotoRating = value;
-                OnPropertyChanged(nameof(NextPhotoRating));
-            }
+            _nextPhotoRating = value;
+            OnPropertyChanged(nameof(NextPhotoRating));
         }
+    }
 
-        private string _nextPhotoDescription;
-        public string NextPhotoDescription
+    private string _nextPhotoDescription;
+    public string NextPhotoDescription
+    {
+        get => _nextPhotoDescription;
+        set
         {
-            get => _nextPhotoDescription;
-            set
-            {
-                _nextPhotoDescription = value;
-                OnPropertyChanged(nameof(NextPhotoDescription));
-            }
+            _nextPhotoDescription = value;
+            OnPropertyChanged(nameof(NextPhotoDescription));
         }
+    }
 
-        public ICommand PreviousPhotoThumbsUpCommand { get; set; }
-        public ICommand PreviousPhotoResetCommand { get; set; }
-        public ICommand PreviousPhotoThumbsDownCommand { get; set; }
+    public ICommand PreviousPhotoThumbsUpCommand { get; set; }
+    public ICommand PreviousPhotoResetCommand { get; set; }
+    public ICommand PreviousPhotoThumbsDownCommand { get; set; }
 
-        public ICommand CurrentPhotoThumbsUpCommand { get; set; }
-        public ICommand CurrentPhotoResetCommand { get; set; }
-        public ICommand CurrentPhotoThumbsDownCommand { get; set; }
+    public ICommand CurrentPhotoThumbsUpCommand { get; set; }
+    public ICommand CurrentPhotoResetCommand { get; set; }
+    public ICommand CurrentPhotoThumbsDownCommand { get; set; }
 
-        public ICommand NextPhotoThumbsUpCommand { get; set; }
-        public ICommand NextPhotoResetCommand { get; set; }
-        public ICommand NextPhotoThumbsDownCommand { get; set; }
+    public ICommand NextPhotoThumbsUpCommand { get; set; }
+    public ICommand NextPhotoResetCommand { get; set; }
+    public ICommand NextPhotoThumbsDownCommand { get; set; }
 
-        public MainViewModel(ApiClientHelper apiClientHelper)
+    public MainViewModel(ApiClientHelper apiClientHelper)
+    {
+        _apiClientHelper = apiClientHelper;
+
+        PreviousPhotoThumbsUpCommand = new Command(OnPreviousPhotoThumbsUp);
+        PreviousPhotoResetCommand = new Command(OnPreviousPhotoReset);
+        PreviousPhotoThumbsDownCommand = new Command(OnPreviousPhotoThumbsDown);
+        CurrentPhotoThumbsUpCommand = new Command(OnCurrentPhotoThumbsUp);
+        CurrentPhotoResetCommand = new Command(OnCurrentPhotoReset);
+        CurrentPhotoThumbsDownCommand = new Command(OnCurrentPhotoThumbsDown);
+        NextPhotoThumbsUpCommand = new Command(OnNextPhotoThumbsUp);
+        NextPhotoResetCommand = new Command(OnNextPhotoReset);
+        NextPhotoThumbsDownCommand = new Command(OnNextPhotoThumbsDown);
+
+        Task.Run(async () =>
         {
-            _apiClientHelper = apiClientHelper;
-
-            PreviousPhotoThumbsUpCommand = new Command(OnPreviousPhotoThumbsUp);
-            PreviousPhotoResetCommand = new Command(OnPreviousPhotoReset);
-            PreviousPhotoThumbsDownCommand = new Command(OnPreviousPhotoThumbsDown);
-            CurrentPhotoThumbsUpCommand = new Command(OnCurrentPhotoThumbsUp);
-            CurrentPhotoResetCommand = new Command(OnCurrentPhotoReset);
-            CurrentPhotoThumbsDownCommand = new Command(OnCurrentPhotoThumbsDown);
-            NextPhotoThumbsUpCommand = new Command(OnNextPhotoThumbsUp);
-            NextPhotoResetCommand = new Command(OnNextPhotoReset);
-            NextPhotoThumbsDownCommand = new Command(OnNextPhotoThumbsDown);
-
-            Task.Run(async () =>
+            do
             {
-                do
+                try
                 {
-                    try
-                    {
-                        await RefreshScheduledPhotos();
-                        //throw new ArgumentNullException(nameof(apiClientHelper));
-                    }
-                    catch (Exception ex)
-                    {
-                        ErrorDescription = ex.Message;
-                    }
+                    await RefreshScheduledPhotos();
                 }
-                while (await _timer.WaitForNextTickAsync());
-            });
-        }
-
-        private async void OnPreviousPhotoThumbsUp(object args)
-        {
-            await SetRating(_previousPhotoId, Rating.ThumbsUp);
-            await RefreshScheduledPhotos();
-        }
-
-        private async void OnPreviousPhotoReset(object args)
-        {
-            await SetRating(_previousPhotoId, Rating.None);
-            await RefreshScheduledPhotos();
-        }
-
-        private async void OnPreviousPhotoThumbsDown(object args)
-        {
-            await SetRating(_previousPhotoId, Rating.ThumbsDown);
-            await RefreshScheduledPhotos();
-        }
-
-        private async void OnCurrentPhotoThumbsUp(object args)
-        {
-            await SetRating(_currentPhotoId, Rating.ThumbsUp);
-            await RefreshScheduledPhotos();
-        }
-
-        private async void OnCurrentPhotoReset(object args)
-        {
-            await SetRating(_currentPhotoId, Rating.None);
-            await RefreshScheduledPhotos();
-        }
-
-        private async void OnCurrentPhotoThumbsDown(object args)
-        {
-            await SetRating(_currentPhotoId, Rating.ThumbsDown);
-            await RefreshScheduledPhotos();
-        }
-
-        private async void OnNextPhotoThumbsUp(object args)
-        {
-            await SetRating(_nextPhotoId, Rating.ThumbsUp);
-            await RefreshScheduledPhotos();
-        }
-
-        private async void OnNextPhotoReset(object args)
-        {
-            await SetRating(_nextPhotoId, Rating.None);
-            await RefreshScheduledPhotos();
-        }
-
-        private async void OnNextPhotoThumbsDown(object args)
-        {
-            await SetRating(_nextPhotoId, Rating.ThumbsDown);
-            await RefreshScheduledPhotos();
-        }
-
-        private async Task SetRating(Guid photoId, Rating rating)
-        {
-            try
-            {
-                await _apiClientHelper.SetRating(new[] { photoId }, rating);
+                catch (Exception ex)
+                {
+                    ErrorDescription = ex.Message;
+                }
             }
-            catch
-            {
-                // Nothing we can do...
-            }
+            while (await _timer.WaitForNextTickAsync());
+        });
+    }
+
+    private async void OnPreviousPhotoThumbsUp(object args)
+    {
+        await SetRating(_previousPhotoId, Rating.ThumbsUp);
+        await RefreshScheduledPhotos();
+    }
+
+    private async void OnPreviousPhotoReset(object args)
+    {
+        await SetRating(_previousPhotoId, Rating.None);
+        await RefreshScheduledPhotos();
+    }
+
+    private async void OnPreviousPhotoThumbsDown(object args)
+    {
+        await SetRating(_previousPhotoId, Rating.ThumbsDown);
+        await RefreshScheduledPhotos();
+    }
+
+    private async void OnCurrentPhotoThumbsUp(object args)
+    {
+        await SetRating(_currentPhotoId, Rating.ThumbsUp);
+        await RefreshScheduledPhotos();
+    }
+
+    private async void OnCurrentPhotoReset(object args)
+    {
+        await SetRating(_currentPhotoId, Rating.None);
+        await RefreshScheduledPhotos();
+    }
+
+    private async void OnCurrentPhotoThumbsDown(object args)
+    {
+        await SetRating(_currentPhotoId, Rating.ThumbsDown);
+        await RefreshScheduledPhotos();
+    }
+
+    private async void OnNextPhotoThumbsUp(object args)
+    {
+        await SetRating(_nextPhotoId, Rating.ThumbsUp);
+        await RefreshScheduledPhotos();
+    }
+
+    private async void OnNextPhotoReset(object args)
+    {
+        await SetRating(_nextPhotoId, Rating.None);
+        await RefreshScheduledPhotos();
+    }
+
+    private async void OnNextPhotoThumbsDown(object args)
+    {
+        await SetRating(_nextPhotoId, Rating.ThumbsDown);
+        await RefreshScheduledPhotos();
+    }
+
+    private async Task SetRating(Guid photoId, Rating rating)
+    {
+        try
+        {
+            await _apiClientHelper.SetRating(new[] { photoId }, rating);
+        }
+        catch
+        {
+            // Nothing we can do...
+        }
+    }
+
+    private async Task RefreshScheduledPhotos()
+    {
+        var previousPhoto = await _apiClientHelper.GetPreviousPhoto();
+        if (previousPhoto.Id != Guid.Empty)
+        {
+            _previousPhotoId = previousPhoto.Id;
+            PreviousPhoto = await _apiClientHelper.GetThumbnail(previousPhoto.Id);
+            PreviousPhotoRating = previousPhoto.Rating;
+            PreviousPhotoDescription = previousPhoto.Description;
+        }
+        else
+        {
+            PreviousPhotoRating = Rating.None;
+            PreviousPhoto = null;
+            PreviousPhotoDescription = string.Empty;
         }
 
-        private async Task RefreshScheduledPhotos()
+        var currentPhoto = await _apiClientHelper.GetCurrentPhoto();
+        if (currentPhoto.Id != Guid.Empty)
         {
-            var previousPhoto = await _apiClientHelper.GetPreviousPhoto();
-            if (previousPhoto.Id != Guid.Empty)
-            {
-                _previousPhotoId = previousPhoto.Id;
-                PreviousPhoto = await _apiClientHelper.GetThumbnail(previousPhoto.Id);
-                PreviousPhotoRating = previousPhoto.Rating;
-                PreviousPhotoDescription = previousPhoto.Description;
-            }
-            else
-            {
-                PreviousPhotoRating = Rating.None;
-                PreviousPhoto = null;
-                PreviousPhotoDescription = string.Empty;
-            }
+            _currentPhotoId = currentPhoto.Id;
+            CurrentPhoto = await _apiClientHelper.GetThumbnail(currentPhoto.Id);
+            CurrentPhotoRating = currentPhoto.Rating;
+            CurrentPhotoDescription = currentPhoto.Description;
+        }
+        else
+        {
+            CurrentPhotoRating = Rating.None;
+            CurrentPhoto = null;
+            CurrentPhotoDescription = string.Empty;
+        }
 
-            var currentPhoto = await _apiClientHelper.GetCurrentPhoto();
-            if (currentPhoto.Id != Guid.Empty)
-            {
-                _currentPhotoId = currentPhoto.Id;
-                CurrentPhoto = await _apiClientHelper.GetThumbnail(currentPhoto.Id);
-                CurrentPhotoRating = currentPhoto.Rating;
-                CurrentPhotoDescription = currentPhoto.Description;
-            }
-            else
-            {
-                CurrentPhotoRating = Rating.None;
-                CurrentPhoto = null;
-                CurrentPhotoDescription = string.Empty;
-            }
-
-            var nextPhoto = await _apiClientHelper.GetNextPhoto();
-            if (nextPhoto.Id != Guid.Empty)
-            {
-                _nextPhotoId = nextPhoto.Id;
-                NextPhoto = await _apiClientHelper.GetThumbnail(nextPhoto.Id);
-                NextPhotoRating = nextPhoto.Rating;
-                NextPhotoDescription = nextPhoto.Description;
-            }
-            else
-            {
-                NextPhotoRating = Rating.None;
-                NextPhoto = null;
-                NextPhotoDescription = string.Empty;
-            }
+        var nextPhoto = await _apiClientHelper.GetNextPhoto();
+        if (nextPhoto.Id != Guid.Empty)
+        {
+            _nextPhotoId = nextPhoto.Id;
+            NextPhoto = await _apiClientHelper.GetThumbnail(nextPhoto.Id);
+            NextPhotoRating = nextPhoto.Rating;
+            NextPhotoDescription = nextPhoto.Description;
+        }
+        else
+        {
+            NextPhotoRating = Rating.None;
+            NextPhoto = null;
+            NextPhotoDescription = string.Empty;
         }
     }
 }
